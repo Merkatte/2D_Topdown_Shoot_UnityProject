40e5c467c0d499693e0b2418f2a4c939faa1e979
Merkatte <66906570+Merkatte@users.noreply.github.com>
2026-01-26 21:42:55 +0900

    Update SpawnPointCalculator.cs

--- Assets/Scripts/Utils/SpawnPointCalculator.cs
+++ Assets/Scripts/Utils/SpawnPointCalculator.cs
@@ -11,13 +11,13 @@ public static class SpawnPointCalculator
     static Vector2 GetRandomSpawnPosition(float xMin, float yMin, float xMax, float yMax, Vector2 playerPos, float radius)
     {
         float maxDistance = GetMaxDistanceFromPlayer(playerPos);
-        
-        
+
+        return Vector2.zero;
     }
 
     static float GetMaxDistanceFromPlayer(Vector2 playerPos)
     {
-        
+        return 0f;
     }
 }
 
